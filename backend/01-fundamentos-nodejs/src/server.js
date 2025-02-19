0b3c87c3e3bcebe3c9ffb3128152bcf935519f5e
Wellington <wellington.map@hotmail.com>
2025-02-19 17:56:08 -0300

    save and select users from local database

--- backend/01-fundamentos-nodejs/src/server.js
+++ backend/01-fundamentos-nodejs/src/server.js
@@ -1,23 +1,28 @@
 import http from 'node:http'
+import { Database } from './database.js'
 
-const users = []
+const database = new Database()
 
 const server = http.createServer((req, res) => {
 
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
+    const users = database.select('users')
+    
     return res
       .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
-    users.push({
+    const user = {
       id: 1,
       name: 'John Doe',
       email: 'johndoe@example.com',
-    })
+    }
+
+    database.insert('users', user)
 
     return res.writeHead(201).end()
   }
